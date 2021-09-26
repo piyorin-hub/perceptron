2533799bd31db6aeb259be92eabc178688d7415c
norito <noripmark@gmail.com>
2021-09-26 23:59:23 +0900

    add gates

--- and_gate.py
+++ and_gate.py
@@ -18,10 +18,11 @@ print(AND(1, 1))
 # 重みとバイアスによる実装
 def bias_and(x1, x2):
     x = np.array([x1, x2])
-    w = np.array([0.5, 0.5]) #重み
-    b = -0.7 #バイアス 
+    w = np.array([0.5, 0.5])
+    b = -0.7
     tmp = np.sum(w * x) + b
     if tmp <= 0:
         return 0
     else:
         return 1
+        
