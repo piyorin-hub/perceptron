51030a84762c84ef599f2e99ffa78ec0e90ccc5d
norito <noripmark@gmail.com>
2021-09-30 23:50:46 +0900

    fix

--- sigmoid.py
+++ sigmoid.py
@@ -3,7 +3,7 @@ import matplotlib.pylab as plt
 
 # シグモイド関数
 def sigmoid(x):
-    return 1 / (1 + np.exp(-1))
+    return 1 / (1 + np.exp(-x))
 
 x = np.arange(-5.0, 5.0, 0.1) # -5.0~5.0を0.1刻みでnumpy 配列生成
 y = sigmoid(x)
